7b7fbd1cd49c2704b7d3d6a2069ba268386fafe3
wonjangcloud9 <48057918+wonjangcloud9@users.noreply.github.com>
2022-07-04 23:44:21 +0900

    RN Layout system

--- App.js
+++ App.js
@@ -3,9 +3,10 @@ import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello! I made a RN App!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1 }}>
+      <View style={{ backgroundColor: "tomato", flex: 1 }}></View>
+      <View style={{ backgroundColor: "teal", flex: 1.5 }}></View>
+      <View style={{ backgroundColor: "orange", flex: 1 }}></View>
     </View>
   );
 }
